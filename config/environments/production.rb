8a915b0430491217431bb905bbf59dcf180cc8af
A Nunez <nuneza73@hotmail.com>
2015-07-19 16:17:03 -0400

    added & setup devise to users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # required for Heroku
+  # set to actual host
+  config.action_mailer.default_url_options = { :host => 'https://med-pinterest.herokuapp.com' } 
 end
