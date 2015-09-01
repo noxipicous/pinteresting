e1d9e840c6bf011009d2429161c7dee4117bf0d6
A Nunez <nuneza73@hotmail.com>
2015-08-31 20:58:24 -0400

    addressed a migration rollback

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150725220719) do
+ActiveRecord::Schema.define(version: 20150729000616) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "user_name"
+    t.text     "body"
+    t.integer  "pin_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
